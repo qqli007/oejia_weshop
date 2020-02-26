2e322632d10e5c7a841b0e34b27ee7afc2e4df01
星空 <qqli007009@gmail.com>
2020-02-26 11:12:29 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'application': True,
     'depends': ['base', 'mail', 'sale'],
     'external_dependencies': {
-        'python': ['pycryptodome', 'xmltodict', 'itsdangerous'],
+        'python': ['Crypto', 'xmltodict', 'itsdangerous'],
     },
     'data': [
         'security/res_groups.xml',
